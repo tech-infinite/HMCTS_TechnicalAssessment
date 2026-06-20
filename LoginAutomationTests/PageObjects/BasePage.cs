ae60d3c012c3e8bcb21052a5587df1c647c78edc
Supraja Ramaraju <test.email@example.com>
2026-06-20 20:48:12 +0100

    Changes to base page and fixture class

--- LoginAutomationTests/PageObjects/BasePage.cs
+++ LoginAutomationTests/PageObjects/BasePage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HMCTS_LoginAutomationTests.Tests;
+using Microsoft.Playwright;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,20 @@ using System.Threading.Tasks;
 
 namespace HMCTS_LoginAutomationTests.PageObjects
 {
-    public class BasePage
+    public abstract class BasePage : IClassFixture<FixtureTests>
     {
+        protected readonly IBrowser Browser;
+        protected const string BaseUrl = "https://the-internet.herokuapp.com";
 
+        protected BasePage(FixtureTests fixture)
+        {
+            Browser = fixture.Browser;
+        }
+
+        protected async Task<IPage> NewPageAsync()
+        {
+            var context = await Browser.NewContextAsync();
+            return await context.NewPageAsync();
+        }
     }
 }
--- LoginAutomationTests/Tests/FixtureTests.cs
+++ LoginAutomationTests/Tests/FixtureTests.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Playwright;
+using Xunit;
 
 namespace HMCTS_LoginAutomationTests.Tests
 {
-    internal class FixtureTests
+    public class FixtureTests : IAsyncLifetime
     {
+        public IPlaywright Playwright { get; private set; } = default!;
+        public IBrowser Browser { get; private set; } = default!;
+
+        public async Task InitializeAsync()
+        {
+            Playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+            Browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = true
+            });
+        }
+
+        public async Task DisposeAsync()
+        {
+            await Browser.CloseAsync();
+            Playwright.Dispose();
+        }
     }
 }
